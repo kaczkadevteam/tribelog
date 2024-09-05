b149e669701c19aaee09045799c5b4cdd2edfacf
Kamil Pula <kamil.pula.dev@gmail.com>
2024-09-05 18:27:12 +0200

    feat: init nuxt app

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+
+  imports: {
+    dirs: [
+      'composables/**',
+      'utils/**',
+    ],
+  },
+
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
+
+  telemetry: false,
+
+  compatibilityDate: '2024-04-03',
+  
+  future: {
+    compatibilityVersion: 4
+  },
+
+  devServer: {
+    port: 3001,
+  },
+})
